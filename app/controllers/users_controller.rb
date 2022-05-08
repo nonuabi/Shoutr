d207fb8c565415fe67ae28eabe3060cb3fcb09ab
abhishek.98 <abhishek.verma@planetspark.in>
2022-05-08 19:01:04 +0530

    Aded User and Shout page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < Clearance::UsersController
+  def show
+    @user = User.find_by(id: params[:id])
+    @shouts = @user.shouts
+  end
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
